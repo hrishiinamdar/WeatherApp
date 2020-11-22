691ea08d3bab6ca6781d76c260cb31a29f49b1bd
Hrishikesh Inamdar <hrishi.inamdar@gmail.com>
2020-11-22 22:31:18 +0530

    first commit

---
+++ WeatherApp/ViewModel/ValueConverters/BoolToRainCoverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WeatherApp.ViewModel.ValueConverters
+{
+    public class BoolToRainCoverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isRaining = (bool)value;
+
+            if (isRaining)
+                return "Currenly Raining";
+
+            return "Currenly Not Raining";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string isRaining = (string)value;
+
+            if (isRaining == "Currenly Raining")
+                return true;
+            return false;
+        }
+    }
+
+}
